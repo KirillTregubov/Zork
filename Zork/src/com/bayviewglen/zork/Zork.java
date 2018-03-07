d5dfb3230aa0751c16046bfc716eaa7b3c968b78
ktregubov <ktregubov@ktregubovsp3.bayviewglen.ca>
2018-03-07 08:24:55 -0500

    0.1-alpha build 1.0.1
    - Fixed a formatting error that imporperly displayed the current date

--- Zork/src/com/bayviewglen/zork/Zork.java
+++ Zork/src/com/bayviewglen/zork/Zork.java
@@ -3,7 +3,7 @@ package com.bayviewglen.zork;
 /** "Zork" Class - the central class of the "Zork" game.
  * Current Authors: Kirill Tregubov, Zacharia Burrafato, Andrew Douglas, Alim Halani
  * Current Version: 0.1-alpha
- * Current Date: March 2018
+ * Current Date: 	March 2018
  */
 
 public class Zork {
